523cb5afc310aad9a35ec5cf15d52e817ff2dd2d
Rik van Straaten <rikvstraaten@gmail.com>
2018-11-12 16:49:00 +0100

    Force HTTPS issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 
     /**
